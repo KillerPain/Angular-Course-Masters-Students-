ce9f9f2cf8bfe67955cbe6a5dfa90ab579989f4e
KillerPain <emailforanypurposes@gmail.com>
2021-11-05 20:58:25 +0600

    init

---
+++ applications/healthcare/src/app/list/list.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListRoutingModule } from './list-routing.module';
+import { ListComponent } from './list/list.component';
+import { PostComponent } from './post/post.component';
+import { DescComponent } from './desc/desc.component';
+import { LayoutComponent } from './layout/layout.component';
+import { CardModule } from 'libraries/ui/src/lib/card/card.module';
+import { ButtonModule } from 'libraries/ui/src/lib/button/button.module';
+import { TextInputModule } from 'libraries/ui/src/lib/text-input';
+
+@NgModule({
+  declarations: [ListComponent, PostComponent, DescComponent, LayoutComponent],
+  imports: [
+    CommonModule,
+    ListRoutingModule,
+    CardModule,
+    ButtonModule,
+    TextInputModule,
+  ],
+})
+export class ListModule {}
--- libraries/ui/src/lib/button/button/button.component.ts
+++ libraries/ui/src/lib/button/button/button.component.ts
@@ -8,6 +8,8 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() color: string = 'primary';
 
+  @Input() disabled: boolean = false;
+
   hover: boolean = false;
 
   @HostListener('mouseenter')
@@ -22,13 +24,20 @@ export class ButtonComponent implements OnInit {
 
   colors = new Map<string, string[]>()
     .set('primary', ['hsl(145, 63%, 49%)', 'hsl(145, 63%, 40%)'])
-    .set('accent', ['hsl(190, 63%, 49%)', 'hsl(190, 63%, 40%)']);
+    .set('accent', ['hsl(190, 63%, 49%)', 'hsl(190, 63%, 40%)'])
+    .set('disabled', ['hsl(0, 0%, 80%)', 'hsl(0, 0%, 60%)']);
 
   constructor() {}
 
   ngOnInit(): void {}
 
   public get Color(): string[] {
+    if (this.disabled) {
+      const res = this.colors.get('disabled');
+      if (res) {
+        return res;
+      }
+    }
     const res = this.colors.get(this.color);
     if (res) {
       return res;
